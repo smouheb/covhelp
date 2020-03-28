358c332050c4b365b0a7ab7ce3a51005fc2b8332
Smael Mouheb <smaelmouheb@Air-de-Smael.fritz.box>
2020-03-28 19:58:39 +0100

    other fixes in terms of layout, the space between the nav bar and the title etc...

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -4,7 +4,10 @@ namespace App\Form;
 
 use App\Entity\Contact;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,11 +16,21 @@ class ContactType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('firstname')
-            ->add('lastname')
-            ->add('email')
-            ->add('subject')
-            ->add('message')
+            ->add('firstname', TextType::class, [
+                'label' =>false
+            ])
+            ->add('lastname', TextType::class, [
+                'label' =>false
+            ])
+            ->add('email', EmailType::class, [
+                'label' => false
+            ])
+            ->add('subject',TextType::class, [
+                'label' =>false
+            ])
+            ->add('message', TextareaType::class, [
+                'label' =>false
+            ])
             ->add('Send', SubmitType::class)
         ;
     }
